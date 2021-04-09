8aa79daadb31bd361112da35992050c473816e8b
Adminis <phudqph09495@fpt.edu.vn>
2021-04-09 12:31:55 +0700

    loading

---
+++ app/src/main/java/com/duan1/app/Loading.java
@@ -0,0 +1,31 @@
+package com.duan1.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class Loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        new CountDownTimer(3000,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent;
+                intent=new Intent(Loading.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        }.start();
+    }
+}
