78234c085f999f6f9bdd1db4e7febc911f4791dd
trinhtuyen <38354287+trinhtuyen@users.noreply.github.com>
2021-04-13 01:12:03 +0700

    update 3.0.0.0.01  (di ngu)

--- app/src/main/java/com/duan1/app/adapter/LichSuAdapter.java
+++ app/src/main/java/com/duan1/app/adapter/LichSuAdapter.java
@@ -27,15 +27,13 @@ public class LichSuAdapter extends RecyclerView.Adapter<LichSuAdapter.LichSuHold
     @NonNull
     @Override
     public LichSuHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_truyen, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_lich_su, parent, false);
         return new LichSuHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull LichSuHolder holder, int position) {
-        holder.textView3.setText(truyenList.get(position).getTen());
-        holder.textView4.setText(truyenList.get(position).getTacGia());
-        holder.imageView.setImageBitmap(truyenList.get(position).getBitmap());
+
     }
 
     @Override
@@ -44,16 +42,13 @@ public class LichSuAdapter extends RecyclerView.Adapter<LichSuAdapter.LichSuHold
     }
 
     public static class LichSuHolder extends RecyclerView.ViewHolder {
-        private ImageView imageView;
-        private TextView textView3;
-        private TextView textView4;
-        private TextView textView5;
+        private ImageView imgXoa;
+        private TextView tvLichsu;
+
         public LichSuHolder(@NonNull View itemView) {
             super(itemView);
-            imageView = (ImageView) itemView.findViewById(R.id.imgAnh);
-            textView3 = (TextView) itemView.findViewById(R.id.tvTentruyen);
-            textView4 = (TextView) itemView.findViewById(R.id.tvTacgia);
-            textView5 = (TextView) itemView.findViewById(R.id.tvTheloai);
+            imgXoa = (ImageView) itemView.findViewById(R.id.imgXoa);
+            tvLichsu = (TextView) itemView.findViewById(R.id.tvLichsu);
         }
     }
 }
